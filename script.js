a0a7747f76ba7af51d5f00f69159f6061d4fc467
marleigh <volcana@gmail.com>
2020-09-01 11:34:55 -0700

    radio button logic

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let distance = 5; /* obviously need to get input eventually */
-let costPerMin = [];
+let costPerMin = [0.2, 0.4, 0.5, 0.8];
 let costPerMile = [];
 let rideTime = 30; /* input needed */
+
+//array: 0: economy, 1: standard car, 2: SUV, 3: luxury
+const distDisplay = document.getElementById('distance');
+
+//get all the radio elements, can then loop through them and figure out which was pushed
+//you can check to see which button is picked by using .checked
+const radio = document.getElementsByName('transport');
